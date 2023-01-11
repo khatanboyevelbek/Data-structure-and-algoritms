fccd57796c8e4d089e9e63b9f4547f0883d29f82
khatanboyevelbek <programmerelbek@gmail.com>
2023-01-11 16:39:29 +0500

    DATA: Add Class Library

---
+++ ClassLibraries/HelperMethods.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibraries
+{
+    public class HelperMethods
+    {
+        public static void Swap<T>(T[] array, int first, int second)
+        {
+            T temp = array[first];
+            array[first] = array[second];
+            array[second] = temp;
+        }
+    }
+}
