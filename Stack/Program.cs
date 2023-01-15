8ec757a0e006b8260a7604fa75f22b57d8aa22a8
khatanboyevelbek <programmerelbek@gmail.com>
2023-01-15 11:12:11 +0500

    DATA: Implament Stack

---
+++ Stack/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Stack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> chars = new Stack<char>();
+
+            foreach (char item in "elbek")
+            {
+                chars.Push(item);
+            }
+
+            while(chars.Count > 0)
+            {
+                Console.WriteLine(chars.Pop());
+            }
+        }
+    }
+}
