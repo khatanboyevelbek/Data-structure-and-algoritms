eaf81e4bc8752a4d3966f936dd687ffccbc9918e
khatanboyevelbek <programmerelbek@gmail.com>
2023-01-17 08:40:44 +0500

    DATA: Implament SortedDictionary

---
+++ SortedDictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace SortedDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, string> definitions = new();
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Choose an option ([a] - add, [l] - list): ");
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+
+                if (consoleKeyInfo.Key == ConsoleKey.A)
+                {
+                    Console.WriteLine("Enter the name: ");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Enter the explanation");
+                    string explanation = Console.ReadLine();
+                    definitions[name] = explanation;
+                }
+                else if (consoleKeyInfo.Key == ConsoleKey.L)
+                {
+                    foreach (KeyValuePair<string, string> definition in definitions)
+                    {
+                        Console.WriteLine($"- {definition.Key}: {definition.Value}");
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            while (true);
+        }
+    }
+}
